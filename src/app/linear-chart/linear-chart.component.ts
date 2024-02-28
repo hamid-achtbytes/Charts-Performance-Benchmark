33492eacba402c0ac2ce2055aa03c256563b4f74
Hamid Hosseini <hhosseini@Stego.de>
2024-02-28 18:18:03 +0100

    Destory chart

--- src/app/linear-chart/linear-chart.component.ts
+++ src/app/linear-chart/linear-chart.component.ts
@@ -15,6 +15,7 @@ export class LinearChartComponent implements AfterViewInit {
     @ViewChild('chart', { static: true }) private chartElement: ElementRef = null!;
 
     private linearChartDataService = inject(LinearChartDataService);
+    private chart?: Chart;
 
     private config: Partial<ChartConfiguration> = {
         type: 'line',
@@ -70,11 +71,11 @@ export class LinearChartComponent implements AfterViewInit {
     public displayChart(seriesCount: any = 10, dataPointsCount: any = 1000): void {
         console.time('Chart render time');
 
+        this.chart?.destroy();
         const config = { ...this.config, data: this.linearChartDataService.getChartData(seriesCount ?? 10, dataPointsCount ?? 100) } as ChartConfiguration;
         const canvas = this.chartElement.nativeElement as HTMLCanvasElement;
         const context = canvas.getContext('2d');
-        context!.clearRect(0, 0, canvas.width, canvas.height);
-        const _ = new Chart(context!, config);
+        this.chart = new Chart(context!, config);
 
         console.timeEnd('Chart render time');
     }
